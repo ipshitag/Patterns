cd0419f5af7bb5bde548d0823b8472b2d372a962
Ipshita Ghosh <iuippi3@gmail.com>
2019-07-07 13:05:59 +0530

    Create asteriskRightAngle.java

---
+++ asteriskRightAngle.java
@@ -0,0 +1,56 @@
+/* 
+ * Enter your code here. Read input from STDIN. Print your output to STDOUT. 
+ * Your class should be named CandidateCode.
+ You need to take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this- 
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+Input Format
+You will take an integer input n from stdin.
+
+Constraints
+1 <= n <= 1000
+
+Output Format
+Your output should be the pattern according to the input which you had entered. 
+
+Sample TestCase 1
+Input
+5
+Output
+* 
+
+* * 
+
+* * * 
+
+* * * * 
+
+* * * * *
+*/
+
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+    public static void main(String args[] ) throws Exception {
+
+    	//Write code here
+        Scanner sc = new Scanner(System.in);
+        int n,i,j;
+        n = sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+                if(j!=i)
+                System.out.print(" ");
+            }
+            if(i!=n)
+            System.out.println();
+        }
+   }
+}
